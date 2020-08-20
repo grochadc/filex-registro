82298c51ede23d910ef3f53f6fc6732ec5c35520
Gonzalo Rocha <grochadc@gmail.com>
2020-08-20 16:16:07 -0500

    useFetch decides if fetch from local db or firebase [untested]

--- src/lib.js
+++ src/lib.js
@@ -8,21 +8,30 @@ const postToDB = (endpoint, data) => {
   return database.ref(endpoint).set(data);
 };
 
-const useFetch = (url, options) => {
+const useFetch = (code, applicant, options) => {
   const [response, setResponse] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [status, setStatus] = useState(null);
+
+  const url = applicant ? `applicants${code}` : `students/${code}`;
 
   useEffect(
     () => {
       const doFetch = async () => {
         setLoading(true);
         try {
-          const res = await fetch(url, options);
-          const json = await res.json();
-          setStatus(res.status);
-          setResponse(json);
+          if (applicant) {
+            //if appllicant fetch firebase
+            const res = await database.ref(url);
+            setResponse(res.val());
+            setStatus("200");
+          } else {
+            const res = await fetch(url, options);
+            const json = await res.json();
+            setStatus(res.status);
+            setResponse(json);
+          }
         } catch (e) {
           console.log(e);
           setError(e);
@@ -32,7 +41,7 @@ const useFetch = (url, options) => {
       };
       doFetch();
     },
-    [url, options]
+    [url, options, applicant]
   );
   return { response, error, loading, status };
 };
