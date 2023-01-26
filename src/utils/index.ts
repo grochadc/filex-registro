5f03106e3024447bd1467465ba5e82b495b5ddaf
Gonzalo Rocha <grochadc@gmail.com>
2023-01-26 17:15:34 -0600

    fix group appears as Object in masterlist

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { EnrolledStudent } from "generated/grapqhl-types";
+
 function parse(array: any[]) {
   const headers = Object.keys(array[0]);
   const table = array.map((row) => {
@@ -6,7 +8,14 @@ function parse(array: any[]) {
   return [headers].concat(table).join("\n");
 }
 
-export function downloadMasterList(masterlist: any[], fileName: string) {
+export function downloadMasterList(
+  masterlist: EnrolledStudent[],
+  fileName: string
+) {
+  const finalMasterList = masterlist.map((student) => ({
+    ...student,
+    group: student.group.name,
+  }));
   const el = document.createElement("a");
   const file = new Blob([parse(masterlist)], { type: "text/csv" });
   el.hidden = true;
