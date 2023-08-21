74f32e43477494dcaa9fddebcb73e2e44f39fe11
Gonzalo Rocha <grochadc@gmail.com>
2023-08-21 10:19:20 -0600

    updates Inicio Clases message

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -36,7 +36,7 @@ const Success = (props: SuccessProps) => {
           </p>
         ) : null}
         <p>Clases de Lunes a Jueves</p>
-        <p>Inicio de Cursos 7 de Febrero</p>
+        <p>Inicio de Cursos 28 de Agosto</p>
       </div>
     </div>
   );
